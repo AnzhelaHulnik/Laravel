37f72b5aa8b526a62e1a1a4f1760500ea91e32cf
AnzhelaHulnik <43501693+AnzhelaHulnik@users.noreply.github.com>
2018-12-17 23:00:23 +0200

    feedback/тестирование почты

--- MailController.php
+++ MailController.php
@@ -2,25 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
+use App\Mail\FeedbackMail;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
     public function feedback(Request $request){
         return view('mails.feedback');
     }
+
+
     public function sendmail(Request $request){
 
 
-       /*dd($request->all());
+       //Product::create($request->all());
+        $feedback = [];
+        $feedback['title'] = $request->input('name');
+        $feedback['content'] = $request->input('content');
+        dd($request->all());
        return response()->json([
            'msg'=> 'Отправка успешна'
-       ]);*/
-       Mail::to('anzgul@tut.by')->send(new email);
+       ]);
+
+
+       Mail::to()->send(new email);
         return response()->json([
             'msg'=> 'Отправка успешна'
         ]);
 
+    }
+
+    public function show(Request $request) {
+        //$result = $request->session()->get('key', 'default');
+      //  $result = $request->session()->all();
+        $request->session()->put('key', 'value');
+        $result = $request->session()->all();
+        dump($result);
     }
 }
